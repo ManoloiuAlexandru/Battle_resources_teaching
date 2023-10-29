2585e149ad189f7af19e6897c921f09e0be483cc
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2023-10-29 16:27:30 +0200

    Add files via upload

--- main_server.py
+++ main_server.py
@@ -4,6 +4,7 @@ from ITschool_projects.battle_resources.clases.creatures import Creature
 from ITschool_projects.battle_resources.clases.game_logics import battle, turn_switch, battle_logic, damage_dealing, \
     damage_to_player
 from ITschool_projects.battle_resources.clases.player import Player
+from ITschool_projects.battle_resources.decks.decks_to_play import starter_deck, demo_deck
 
 app = Flask(__name__)
 global player1
@@ -21,23 +22,19 @@ def game_start():
             attacked_player = 2
             player1 = Player("Alex")
             player1.turn = 1
-            player1.hand = [Creature(1, "Peasant", 1, 1, None, 1),
-                            Creature(1, "Peasant", 1, 1, None, 2)]
+            player1.hand = demo_deck
             player1.mana = 1
             player2 = Player("Andras")
-            player2.hand = [Creature(1, "Peasant", 1, 1, None, 3),
-                            Creature(1, "Knight", 3, 3, None, 4)]
+            player2.hand = starter_deck
             player2.mana = -100
     except Exception as e:
         attacked_player = 2
         player1 = Player("Alex")
-        player1.hand = [Creature(1, "Peasant", 1, 1, None, 1),
-                        Creature(1, "Peasant", 1, 1, None, 2)]
+        player1.hand = demo_deck
         player1.turn = 1
-        player1.mana = 100
+        player1.mana = 10
         player2 = Player("Andras")
-        player2.hand = [Creature(1, "Peasant", 1, 1, None, 3),
-                        Creature(1, "Knight", 3, 3, None, 4)]
+        player2.hand = starter_deck
         player2.mana = 10
 
 
@@ -68,16 +65,20 @@ def battlefield_fight():
     global current_card
     global player2
     global player1
+    player_selected = False
     if player1.turn == 1:
         current_card = battle_logic(player1, card_picked)
     elif attacked_player == 2:
         card = damage_dealing(player2, card_picked)
         if card is not None:
             battle(current_card, card, player1, player2)
+            current_card = None
         elif card_picked.get(player2.name) is not None:
             current_card.exhausted = True
             player2, current_card = damage_to_player(player2, current_card)
+            player_selected = True
             player1.turn = 1
+            current_card = None
             if player2.check_player() == 0:
                 return render_template("END.html", player=player2)
     if player2.turn == 1:
@@ -90,8 +91,17 @@ def battlefield_fight():
         elif card_picked.get(player1.name) is not None:
             player1, current_card = damage_to_player(player1, current_card)
             player2.turn = 1
+            player_selected = True
             if player1.check_player() == 0:
                 return render_template("END.html", player=player1)
+    if card_picked is None or player_selected is not True:
+        if attacked_player == 1:
+            player2.problem = "You need to select a card"
+        else:
+            player1.problem = "You need to select a card"
+    else:
+        player1.problem = ""
+        player2.problem = ""
     return redirect(url_for('show_battle', players=[player2, player1]))
 
 
