3f56eb87a511a584b3e24bda0784d266fccde9b9
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2023-10-30 08:51:22 +0200

    Create decks_to_play.py

---
+++ decks/decks_to_play.py
@@ -0,0 +1,39 @@
+from ITschool_projects.battle_resources.clases.creatures import Creature
+
+starter_deck = [Creature(1, "Peasant", 1, 1, "", 1),
+                Creature(1, "Peasant", 1, 1, "", 2),
+                Creature(1, "Peasant", 1, 1, "", 3),
+                Creature(1, "Peasant", 1, 1, "", 4),
+                Creature(1, "Peasant", 1, 1, "", 5),
+                Creature(1, "Peasant", 1, 1, "", 6)]
+
+demo_deck = [Creature(2, "Archer", 1, 2, "", 7),
+             Creature(7, 'Templar Knight', 5, 5, "", 8),
+             Creature(3, "Spearmen", 3, 2, "", 9),
+             Creature(2, "Militia", 2, 2, "", 10),
+             Creature(5, "Crossbowmen", 4, 6, "", 11),
+             Creature(1, "Peasant", 1, 1, "", 12),
+             Creature(3, "Guard", 4, 1, "Guard", 13)]
+
+test_deck = [Creature(3, "Guard", 4, 1, "Guard", 14),
+             Creature(3, "Guard", 4, 1, "Guard", 15),
+             Creature(3, "Guard", 4, 1, "Guard", 16),
+             Creature(3, "Guard", 4, 1, "Guard", 17),
+             Creature(3, "Guard", 4, 1, "Guard", 18),
+             Creature(3, "Guard", 4, 1, "Guard", 19),
+             Creature(3, "Guard", 4, 1, "Guard", 20),
+             Creature(3, "Guard", 4, 1, "Guard", 21),
+             Creature(3, "Guard", 4, 1, "Guard", 22)]
+
+knight_deck = [Creature(6, 'Templar Knight', 5, 5, "", 23),
+               Creature(1, 'Page', 1, 1, "", 24),
+               Creature(3, 'Squire', 1, 1, "", 25),
+               Creature(3, 'Squire', 1, 1, "", 26),
+               Creature(6, 'Templar Knight', 5, 5, "", 27),
+               Creature(6, 'Templar Knight', 5, 5, "", 28),
+               Creature(8, 'Hospitaller Knight', 8, 3, "", 29),
+               Creature(8, 'Hospitaller Knight', 8, 3, "", 30),
+               Creature(8, 'Hospitaller Knight', 8, 3, "", 31),
+               Creature(7, 'Teutonic Knight', 6, 5, "", 32),
+               Creature(7, 'Teutonic Knight', 6, 5, "", 33),
+               ]
