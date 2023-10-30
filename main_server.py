f88bed027b9e919caf2e790d6d712526cce14f4c
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2023-10-30 10:36:21 +0200

    Add files via upload

--- main_server.py
+++ main_server.py
@@ -3,9 +3,9 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 from ITschool_projects.battle_resources.clases.bot import Bot
 from ITschool_projects.battle_resources.clases.creatures import Creature
 from ITschool_projects.battle_resources.clases.game_logics import battle, turn_switch, battle_logic, damage_dealing, \
-    damage_to_player
+    damage_to_player, guard_checking, destroy_creature, check_target
 from ITschool_projects.battle_resources.clases.player import Player
-from ITschool_projects.battle_resources.decks.decks_to_play import starter_deck, demo_deck
+from ITschool_projects.battle_resources.decks.decks_to_play import starter_deck, demo_deck, test_deck
 
 app = Flask(__name__)
 global player1
@@ -38,7 +38,7 @@ def game_start():
         # player2.hand = starter_deck
         # player2.mana = 10
         player2 = Bot("Bot")
-        player2.hand = starter_deck
+        player2.hand = test_deck
         player2.mana = 10
 
 
@@ -54,31 +54,47 @@ def update_battle():
     global player2
     if attacked_player == 2:
         card_picked = request.form
-        if player1.put_card_on_field(card_picked) == 0:
+        if player1.put_card_on_field(card_picked) == 2:
+            return redirect(url_for('battlefield_fight'))
+        elif player1.put_card_on_field(card_picked) == 0:
             player1.problem = "Not enough mana"
     elif type(player2) == Player:
         card_picked = request.form
-        if player2.put_card_on_field(card_picked) == 0:
+        if player2.put_card_on_field(card_picked) == 2:
+            return redirect(url_for('battlefield_fight'))
+        elif player2.put_card_on_field(card_picked) == 0:
             player2.problem = "No enough mana"
     return redirect(url_for('show_battle', players=[player2, player1], attacking_card=None))
 
 
-@app.route("/battlefield_action", methods=["POST"])
+@app.route("/battlefield_action", methods=["POST", "GET"])
 def battlefield_fight():
     card_picked = request.form
     global current_card
     global player2
     global player1
     player_selected = False
+    if player1.incoming_action == 2:
+        if check_target(player1, player2, card_picked) == 0:
+            player1.problem = "You need to select a card"
+        else:
+            destroy_creature(card_picked, player2)
+            player1.incoming_action = 0
+    if player2.incoming_action == 2:
+        if check_target(player2, player1, card_picked) == 0:
+            player2.problem = "You need to select a card"
+        else:
+            destroy_creature(card_picked, player1)
+            player2.incoming_action = 0
     if player1.turn == 1:
         current_card = battle_logic(player1, card_picked)
     elif attacked_player == 2:
         card = damage_dealing(player2, card_picked)
-        if card is not None:
+        if card is not None and guard_checking(player2, card) == 1:
             battle(current_card, card, player1, player2)
             player1.turn = 1
             current_card = None
-        elif card_picked.get(player2.name) is not None:
+        elif card_picked.get(player2.name) is not None and guard_checking(player2, card) == 1:
             current_card.exhausted = True
             player2, current_card = damage_to_player(player2, current_card)
             player_selected = True
@@ -86,19 +102,25 @@ def battlefield_fight():
             current_card = None
             if player2.check_player() == 0:
                 return render_template("END.html", player=player2)
+        elif guard_checking(player2, card) == 0:
+            player1.problem = "There are guards in battle"
+            return redirect(url_for('show_battle', players=[player2, player1]))
     if player2.turn == 1:
         current_card = battle_logic(player2, card_picked)
     elif attacked_player == 1:
         card = damage_dealing(player1, card_picked)
-        if card is not None:
+        if card is not None and guard_checking(player1, card) == 1:
             battle(current_card, card, player2, player1)
             player2.turn = 1
-        elif card_picked.get(player1.name) is not None:
+        elif card_picked.get(player1.name) is not None and guard_checking(player1, card) == 1:
             player1, current_card = damage_to_player(player1, current_card)
             player2.turn = 1
             player_selected = True
             if player1.check_player() == 0:
                 return render_template("END.html", player=player1)
+        elif guard_checking(player1, card) == 0:
+            player2.problem = "There are guards in battle"
+            return redirect(url_for('show_battle', players=[player2, player1]))
     if card_picked is None or player_selected is not True:
         if attacked_player == 1:
             player2.problem = "You need to select a card"
