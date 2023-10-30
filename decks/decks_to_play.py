77228c5b4b0864f1b6c4f40ba57ea5b73af7173e
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2023-10-30 10:36:40 +0200

    Add files via upload

--- decks/decks_to_play.py
+++ decks/decks_to_play.py
@@ -5,7 +5,8 @@ starter_deck = [Creature(1, "Peasant", 1, 1, "", 1),
                 Creature(1, "Peasant", 1, 1, "", 3),
                 Creature(1, "Peasant", 1, 1, "", 4),
                 Creature(1, "Peasant", 1, 1, "", 5),
-                Creature(1, "Peasant", 1, 1, "", 6)]
+                Creature(1, "Peasant", 1, 1, "", 6),
+                Creature(0, "Andras", 99, 99, "", 1000)]
 
 demo_deck = [Creature(2, "Archer", 1, 2, "", 7),
              Creature(7, 'Templar Knight', 5, 5, "", 8),
@@ -13,17 +14,20 @@ demo_deck = [Creature(2, "Archer", 1, 2, "", 7),
              Creature(2, "Militia", 2, 2, "", 10),
              Creature(5, "Crossbowmen", 4, 6, "", 11),
              Creature(1, "Peasant", 1, 1, "", 12),
-             Creature(3, "Guard", 4, 1, "Guard", 13)]
+             Creature(3, "Guard", 4, 1, "Guard", 13),
+             Creature(8, 'Hospitaller Knight', 8, 3, "", 30),
+             Creature(8, 'Hospitaller Knight', 8, 3, "", 31),
+             Creature(7, 'Teutonic Knight', 6, 5, "", 32),
+             Creature(7, 'Teutonic Knight', 6, 5, "", 33),
+             Creature(9, "Two-handed Knight", 5, 5, "Kill an enemy soldier", 20),
+             Creature(9, "Two-handed Knight", 5, 5, "Kill an enemy soldier", 21),
+             ]
 
-test_deck = [Creature(3, "Guard", 4, 1, "Guard", 14),
+test_deck = [Creature(3, "Guard", 4, 1, "Guard", 13),
+             Creature(3, "Guard", 4, 1, "Guard", 14),
              Creature(3, "Guard", 4, 1, "Guard", 15),
              Creature(3, "Guard", 4, 1, "Guard", 16),
-             Creature(3, "Guard", 4, 1, "Guard", 17),
-             Creature(3, "Guard", 4, 1, "Guard", 18),
-             Creature(3, "Guard", 4, 1, "Guard", 19),
-             Creature(3, "Guard", 4, 1, "Guard", 20),
-             Creature(3, "Guard", 4, 1, "Guard", 21),
-             Creature(3, "Guard", 4, 1, "Guard", 22)]
+             Creature(3, "Guard", 4, 1, "Guard", 17),]
 
 knight_deck = [Creature(6, 'Templar Knight', 5, 5, "", 23),
                Creature(1, 'Page', 1, 1, "", 24),
@@ -36,4 +40,7 @@ knight_deck = [Creature(6, 'Templar Knight', 5, 5, "", 23),
                Creature(8, 'Hospitaller Knight', 8, 3, "", 31),
                Creature(7, 'Teutonic Knight', 6, 5, "", 32),
                Creature(7, 'Teutonic Knight', 6, 5, "", 33),
+               Creature(9, "Two-handed Knight", 5, 5, "Kill an enemy soldier", 20),
+               Creature(9, "Two-handed Knight", 5, 5, "Kill an enemy soldier", 21),
+               Creature(9, "Two-handed Knight", 5, 5, "Kill an enemy soldier", 22),
                ]
