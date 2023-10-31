07f334300a8b7bb128dfdabe3312c6927c70b05a
ManoloiuAlexandru <34137425+ManoloiuAlexandru@users.noreply.github.com>
2023-10-31 11:51:02 +0200

    Add files via upload

--- decks/decks_to_play.py
+++ decks/decks_to_play.py
@@ -8,7 +8,10 @@ starter_deck = [Creature(1, "Peasant", 1, 1, "", 1),
                 Creature(1, "Peasant", 1, 1, "", 5),
                 Creature(1, "Peasant", 1, 1, "", 6),
                 Creature(0, "Andras", 99, 99, "", 1000)]
-
+bot_deck = [Creature(7, 'Templar Knight', 7, 5, "Guard", 261),
+            Creature(7, 'Templar Knight', 7, 5, "Guard", 262),
+            Creature(4, "Mounted Knight", 4, 4, "Charge", 263),
+            Creature(4, "Mounted Knight", 4, 4, "Charge", 264), ]
 demo_deck = [Creature(1, 'Hospitaller Knight', 5, 4, "Heal +2 a friendly soldier", 1),
              Creature(1, 'Hospitaller Knight', 5, 4, "Heal +2 a friendly soldier", 2),
              Creature(1, 'Hospitaller Knight', 5, 4, "Heal +2 a friendly soldier", 3),
@@ -19,6 +22,16 @@ demo_deck = [Creature(1, 'Hospitaller Knight', 5, 4, "Heal +2 a friendly soldier
              Creature(4, "Mounted Knight", 4, 4, "Charge", 15),
              Creature(4, "Mounted Knight", 4, 4, "Charge", 16),
              ]
+deck_for_images = [Creature(1, 'Hospitaller Knight', 5, 4, "Heal +2 a friendly soldier", 1),
+                   Creature(1, 'Hospitaller Knight', 5, 4, "Heal +2 a friendly soldier", 2),
+                   Creature(1, 'Hospitaller Knight', 5, 4, "Heal +2 a friendly soldier", 3),
+                   Creature(1, 'Hospitaller Knight', 5, 4, "Heal +2 a friendly soldier", 4),
+                   Creature(1, 'Hospitaller Knight', 5, 4, "Heal +2 a friendly soldier", 5),
+                   Creature(1, 'Hospitaller Knight', 5, 4, "Heal +2 a friendly soldier", 5),
+                   Creature(1, 'Hospitaller Knight', 5, 4, "Heal +2 a friendly soldier", 5),
+                   Creature(1, 'Hospitaller Knight', 5, 4, "Heal +2 a friendly soldier", 5),
+                   Creature(1, 'Hospitaller Knight', 5, 4, "Heal +2 a friendly soldier", 5),
+                   Creature(1, 'Hospitaller Knight', 5, 4, "Heal +2 a friendly soldier", 5), ]
 
 test_deck = [Creature(1, 'Hospitaller Knight', 5, 4, "Heal 2 a friendly soldier", 6),
              Creature(1, 'Hospitaller Knight', 5, 4, "Heal 2 a friendly soldier", 7),
@@ -26,9 +39,13 @@ test_deck = [Creature(1, 'Hospitaller Knight', 5, 4, "Heal 2 a friendly soldier"
              Creature(1, 'Hospitaller Knight', 5, 4, "Heal 2 a friendly soldier", 9),
              Spell(1, "Arrow shot", "Deal 1 damage to a enemy", 11),
              Spell(1, "Arrow shot", "Deal 1 damage to a enemy", 12),
-             Creature(4, "Mounted Knight", 4, 4, "Charge", 13),
-             Creature(4, "Mounted Knight", 4, 4, "Charge", 14),
+             Creature(1, "Mounted Knight", 4, 4, "Charge", 13),
+             Creature(1, "Mounted Knight", 4, 4, "Charge", 14),
              Creature(1, 'Templar Knight', 10, 5, "Guard", 261),
+             Creature(1, 'Hospitaller Knight', 5, 4, "Heal 2 a friendly soldier", 15),
+             Creature(1, 'Hospitaller Knight', 5, 4, "Heal 2 a friendly soldier", 16),
+             Creature(1, 'Hospitaller Knight', 5, 4, "Heal 2 a friendly soldier", 17),
+             Creature(1, 'Hospitaller Knight', 5, 4, "Heal 2 a friendly soldier", 18),
              ]
 
 knight_deck = [Creature(6, 'Templar Knight', 5, 5, "", 23),
@@ -51,8 +68,8 @@ knight_deck_official = [Creature(9, "Two-handed Knight", 5, 5, "Kill an enemy so
                         Creature(9, "Two-handed Knight", 5, 5, "Kill an enemy soldier", 252),
                         Creature(7, 'Teutonic Knight', 6, 5, "", 253),
                         Creature(7, 'Teutonic Knight', 6, 5, "", 254),
-                        Creature(6, 'Hospitaller Knight', 5, 4, "Heal +2 a friendly soldier", 255),
-                        Creature(6, 'Hospitaller Knight', 5, 4, "Heal +2 a friendly soldier", 256),
+                        Creature(6, 'Hospitaller Knight', 5, 4, "Heal a friendly soldier for 2", 255),
+                        Creature(6, 'Hospitaller Knight', 5, 4, "Heal a friendly soldier for 2", 256),
                         Creature(2, 'Squire', 2, 2, "", 257),
                         Creature(2, 'Squire', 2, 2, "", 258),
                         Creature(1, 'Page', 1, 1, "", 259),
@@ -68,3 +85,11 @@ knight_deck_official = [Creature(9, "Two-handed Knight", 5, 5, "Kill an enemy so
                         Spell(5, "Kill", "Kill an enemy soldier", 269),
                         Spell(5, "Kill", "Kill an enemy soldier", 270),
                         ]
+
+to_be_added_in_game = [Spell(3, "Bandage", "Heal a friendly soldier for 4", 1000),
+                       Spell(4, "Bandages", "Restore a friendly soldier to full HP", 1001),
+                       Spell(5, "In the name of the king", "All friendly soldiers get +1/+1", 1002),
+                       Creature(3, "Priest", 1, 1, "Restore a friendly soldier to full HP", 1003),
+                       Spell(0, "Blood price", "Take 5 dmg and gaine 3 mana", 1004),
+                       Spell(3, "Long march", "All enemies are exhausted", 1005),
+                       Creature(9, "Heavy Cavalary", 9, 9, "Takes 2 less fmg from attacks", 1006)]
