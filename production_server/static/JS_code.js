f3301a8645d95f23cdba2f3e091b8385475b7f86
ManoloiuAlexandru <manalexio@yahoo.com>
2023-11-12 11:13:56 +0200

    graphics

---
+++ production_server/static/JS_code.js
@@ -0,0 +1,14 @@
+function disable_all() {
+          var x = document.getElementById("reset_button");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+  var x = document.getElementById("hide_button");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+        }
